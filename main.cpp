f72645cbb64ec418c8b5d544b7dc488260114475
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-21 22:32:00 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 ﻿#include <iostream>
-using namespace std;
+#include <conio.h>
+#include <Windows.h>
 
-int main() {
+using namespace std;
+HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+
+
+
+int main()
+{
+	SetConsoleTextAttribute(hConsole, 10);
+	cout << "\"U lukomor'a dub zelenij," << endl;
+	cout << " Zlataya zep na dupe tom," << endl;
+	cout << " I dnem i nochju kot uchenij" << endl;
+	cout << " vse hodit po cepi krugom\"" << endl;
+	SetConsoleTextAttribute(hConsole, 7);
 	return 0;
 }
