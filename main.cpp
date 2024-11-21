63397597da331260e07cda38a5c1f38770487920
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-21 22:46:29 +0200

    completed task 2

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
-﻿#include <iostream>
+﻿#define _USE_MATH_DEFINES
+#include <iostream>
+#include <cmath>
 using namespace std;
 
 int main() {
-	return 0;
+    double L, R, S;
+    cout << "L: ";
+    cin >> L;
+    R = L / (2 * M_PI);
+    S = M_PI * R * R;
+    cout << "Square: " << S << endl;
+    return 0;
 }
