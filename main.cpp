21155658b6776d4f86e0a0ea32e6ebe2cafa6831
Maris Shardyko <marrttaoburberry@gmail.com>
2024-11-21 22:42:25 +0200

    completed task 1

--- main.cpp
+++ main.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int main() {
-	return 0;
+    double R1, R2, R3, R0;
+    cout << "Write R1, R2, R3: ";
+    cin >> R1;
+    cin >> R2;
+    cin >> R3;
+    R0 = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+    cout << "R0: " << R0 << endl;
+    return 0;
 }
